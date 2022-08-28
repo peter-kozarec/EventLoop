183f0472ddc46a3d9b66f4165a4084ad312bc384
Peter Kozarec <peter.kozarec@outlook.sk>
2022-08-28 13:28:36 +0200

    Removed not supported systems

--- src/utils/compat.hpp
+++ src/utils/compat.hpp
@@ -3,30 +3,32 @@
 
 #if defined(_WIN32) || defined(_WIN64) || (defined(__CYGWIN__) && !defined(_WIN32))
 #include "windows.h"
+/*
 #elif defined(__ANDROID__)
-    #define PLATFORM_NAME "android" // Android (implies Linux, so it must come first)
+    #define PLATFORM_NAME "android"
 #elif defined(__linux__)
-    #define PLATFORM_NAME "linux" // Debian, Ubuntu, Gentoo, Fedora, openSUSE, RedHat, Centos and other
+    #define PLATFORM_NAME "linux"
 #elif defined(__unix__) || !defined(__APPLE__) && defined(__MACH__)
     #include <sys/param.h>
     #if defined(BSD)
-        #define PLATFORM_NAME "bsd" // FreeBSD, NetBSD, OpenBSD, DragonFly BSD
+        #define PLATFORM_NAME "bsd"
     #endif
 #elif defined(__hpux)
-    #define PLATFORM_NAME "hp-ux" // HP-UX
+    #define PLATFORM_NAME "hp-ux"
 #elif defined(_AIX)
-    #define PLATFORM_NAME "aix" // IBM AIX
-#elif defined(__APPLE__) && defined(__MACH__) // Apple OSX and iOS (Darwin)
+    #define PLATFORM_NAME "aix"
+#elif defined(__APPLE__) && defined(__MACH__)
     #include <TargetConditionals.h>
     #if TARGET_IPHONE_SIMULATOR == 1
-        #define PLATFORM_NAME "ios" // Apple iOS
+        #define PLATFORM_NAME "ios"
     #elif TARGET_OS_IPHONE == 1
-        #define PLATFORM_NAME "ios" // Apple iOS
+        #define PLATFORM_NAME "ios"
     #elif TARGET_OS_MAC == 1
-        #define PLATFORM_NAME "osx" // Apple OSX
+        #define PLATFORM_NAME "osx"
     #endif
 #elif defined(__sun) && defined(__SVR4)
-    #define PLATFORM_NAME "solaris" // Oracle Solaris, Open Indiana
+    #define PLATFORM_NAME "solaris"
+*/
 #else
     #error "Unknown os"
 #endif
